bd27d7ca8cb080c03fc769ef83cc171756b44e08
Muhammad Habibullah <mhabibullah14@yahoo.com>
2020-02-23 20:40:46 +0700

    Add second binary deployment

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,12 @@
 echo "Export env..."
 source ./env.sh
 
-echo "Building binary..."
-GOOS=linux GOARCH=amd64 go build -o bin/ta2-script
-echo "Sending binary..."
-scp -P 8288 bin/ta2-script root@$SERVER_HOST:/usr/local/bin
+echo "Building first binary..."
+GOOS=linux GOARCH=amd64 go build -o bin/ta2-script-raw
+echo "Sending first binary..."
+scp -P 8288 bin/ta2-script-raw root@$SERVER_HOST:/usr/local/bin
+
+echo "Building second binary..."
+GOOS=linux GOARCH=amd64 go build -o bin/ta2-script-fin
+echo "Sending second binary..."
+scp -P 8288 bin/ta2-script-fin root@$SERVER_HOST:/usr/local/bin
