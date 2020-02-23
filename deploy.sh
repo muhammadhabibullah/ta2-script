20f10a26658c66389f78452f2e3f87141c5cef30
Muhammad Habibullah <mhabibullah14@yahoo.com>
2020-02-23 19:56:55 +0700

    Fix build script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,7 @@
 echo "Export env..."
 source ./env.sh
 
-echo "Building raw script..."
-GOOS=linux GOARCH=amd64 go build -o bin/ta2-script-raw
-echo "Sending raw script..."
-scp -P 8288 bin/ta2-script-raw root@$SERVER_HOST:/usr/local/bin
-
-echo "Building finale script..."
-GOOS=linux GOARCH=amd64 go build -o bin/ta2-script-fin
-echo "Sending finale script..."
-scp -P 8288 bin/ta2-script-fin root@$SERVER_HOST:/usr/local/bin
+echo "Building binary..."
+GOOS=linux GOARCH=amd64 go build -o bin/ta2-script
+echo "Sending binary..."
+scp -P 8288 bin/ta2-script root@$SERVER_HOST:/usr/local/bin
